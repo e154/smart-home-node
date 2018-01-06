e3ba8c82653142d9cf2d81a53d5b5b81da2dbcf4
praetorian <filippov.a@e154.ru>
2018-01-07 00:58:07 +0700

    node system refactor

--- server/server.go
+++ server/server.go
@@ -5,26 +5,50 @@ import (
 	"fmt"
 	"log"
 	"net/rpc"
+	"sync"
+	"time"
+	"github.com/e154/smart-home-node/serial"
 )
 
+const (
+	TIME_TICK = 30 * time.Second
+)
+
+var (
+	Server *server
+)
+
+type Queue struct {
+	Dev		string
+	Cb		func(thread *Thread)
+}
+
 func Start(addr string, port int) {
-	server := &Server{
-		Addr: addr,
-		Port: port,
-		clients: make(map[*Client]bool),
+	Server = &server{
+		Addr:       addr,
+		Port:       port,
+		lastUpdate: time.Now(),
+		clients:    make(map[*Client]bool),
+		pool:       make(map[string]*Thread),
 	}
 
-	server.Start()
+	Server.UpdateThreads()
+	Server.run()
 }
 
-type Server struct {
-	Addr	string
-	Port 	int
-	listener net.Listener
-	clients map[*Client]bool
+type server struct {
+	Addr       string
+	Port       int
+	listener   net.Listener
+	clients    map[*Client]bool
+	sync.RWMutex
+	lastUpdate time.Time
+	deviceList []string
+	pool       Threads
+	queue	   sync.Map
 }
 
-func (s *Server) Start() (err error) {
+func (s *server) run() (err error) {
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", s.Addr, s.Port))
 	if err != nil {
@@ -39,7 +63,7 @@ func (s *Server) Start() (err error) {
 	rpc.Register(&Smartbus{})
 	rpc.Register(&Node{})
 
-	log.Printf("Start server on %s:%d\r\n", s.Addr, s.Port)
+	log.Printf("start server on %s:%d\r\n", s.Addr, s.Port)
 
 	go func() {
 		for {
@@ -48,10 +72,21 @@ func (s *Server) Start() (err error) {
 		}
 	}()
 
+	go func() {
+		ticker := time.Tick(TIME_TICK)
+		for {
+			select {
+			case <-ticker:
+				s.UpdateThreads()
+				s.UpdateCLientQueue()
+			}
+		}
+	}()
+
 	return
 }
 
-func (s *Server) AddClient(conn net.Conn) {
+func (s *server) AddClient(conn net.Conn) {
 
 	addr := conn.RemoteAddr().String()
 	log.Printf("New client %s\r\n", addr)
@@ -65,4 +100,106 @@ func (s *Server) AddClient(conn net.Conn) {
 	}()
 
 	client.listener(conn)
+}
+
+// обновление пула активных устройств
+//
+func (s *server) UpdateThreads() {
+
+	s.Lock()
+	defer s.Unlock()
+
+	s.deviceList = serial.DeviceList()
+
+	//remove threads
+	for pKey, thread := range s.pool {
+		var exist bool
+		for _, dev := range s.deviceList {
+			if dev == pKey {
+				exist = true
+			}
+		}
+		if exist {
+			continue
+		}
+		thread.Remove()
+		fmt.Println("Remove device from pool:", pKey)
+		delete(s.pool, pKey)
+	}
+
+	for _, dev := range s.deviceList {
+		if _, ok := s.pool[dev]; ok {
+			continue
+		}
+
+		fmt.Println("Add device to pool:", dev)
+		s.pool[dev] = NewThread(dev)
+	}
+}
+
+func (s *server) GetThread(dev string) (*Thread, ThreadState) {
+
+	s.Lock()
+	defer s.Unlock()
+
+	//если нет портов
+	if len(s.pool) == 0 {
+		return nil, THREAD_DEV_NOT_FOUND
+	}
+
+	//выбор порта по названию
+	if dev != "" {
+		if thread, ok := s.pool[dev]; ok {
+			if thread.Busy {
+				return nil, THREAD_BUSY
+			}
+
+			return thread, THREAD_OK
+		}
+		//по названию не найдено
+		return nil, THREAD_DEV_NOT_FOUND
+	}
+
+	//выбор свободного порта
+	for _, thread := range s.pool {
+		if thread.Busy {
+			continue
+		}
+
+		return thread, THREAD_OK
+	}
+
+	//если все порты заняты
+	if len(s.pool) > 0 {
+		return nil, THREAD_ALL_BUSY
+	}
+
+	return nil, THREAD_NOT_FOUND
+}
+
+func (s *server) ClientQueue(addr int, dev string,f func(thread *Thread)) {
+
+	s.queue.Store(addr, &Queue{dev, f})
+
+	s.UpdateCLientQueue()
+}
+
+func (s *server) UpdateCLientQueue() {
+
+	s.queue.Range(func(key, value interface{}) bool {
+
+		cli := value.(*Queue)
+
+		thread, status := s.GetThread(cli.Dev)
+		if status == THREAD_OK {
+			go cli.Cb(thread)
+			s.queue.Delete(key)
+		}
+
+		return true
+	})
+}
+
+func (s *server) ThreadReady(thread *Thread) {
+	s.UpdateCLientQueue()
 }
--- server/smartbus.go
+++ server/smartbus.go
@@ -1,15 +1,10 @@
 package server
 
 import (
-	. "github.com/e154/smart-home-node/cache"
-	"github.com/e154/smart-home-node/serial"
-	. "github.com/e154/smart-home-node/settings"
-	"github.com/e154/smart-home-node/serial/smartbus"
-	"fmt"
-	"errors"
-	"encoding/hex"
-	"sync"
 	"time"
+	"fmt"
+	"github.com/pkg/errors"
+	. "github.com/e154/smart-home-node/cache"
 )
 
 const (
@@ -19,142 +14,74 @@ const (
 var cache *Cache
 
 type Request struct {
-	Line		string		`json: "line"`
-	Device		string		`json: "device"`
-	Baud		int		`json: "baud"`
-	StopBits	int		`json: "stop_bits"`
-	Sleep		int64		`json: "sleep"`
-	Timeout		time.Duration	`json: "timeout"`
-	Command		[]byte		`json: "command"`
-	Result		bool		`json: "result"`
+	Line     string        `json: "line"`
+	Device   string        `json: "device"`
+	Baud     int           `json: "baud"`
+	StopBits int           `json: "stop_bits"`
+	Sleep    int64         `json: "sleep"`
+	Timeout  time.Duration `json: "timeout"`
+	Command  []byte        `json: "command"`
+	Result   bool          `json: "result"`
 }
 
 type Result struct {
-	Command   []byte		`json: "command"`
-	Device    string		`json: "device"`
-	Result    string		`json: "result"`
-	Error     string		`json: "error"`
-	ErrorCode string		`json: "error_code"`
+	Command   []byte `json: "command"`
+	Device    string `json: "device"`
+	Result    string `json: "result"`
+	Error     string `json: "error"`
+	ErrorCode string `json: "error_code"`
 }
 
 type Smartbus struct {
-	mu	sync.Mutex
+
 }
 
 func (m *Smartbus) Send(request *Request, result *Result) error {
 
-	m.mu.Lock()
-	defer m.mu.Unlock()
-
 	if len(request.Command) == 0 {
-		return errors.New("command == []")
+		return errors.New("bad command")
 	}
 
-	conn := &serial.Serial{
-		Dev: "",
-		Baud: AppConfig.Baud,
-		ReadTimeout: AppConfig.Timeout,
-		StopBits: AppConfig.StopBits,
-	}
+	//fmt.Println("---", request.Command)
 
-	if request.Device != "" {
-		conn.Dev = request.Device
-	}
+	addr := int(request.Command[ADDRESS])
+	cacheKey := cache.GetKey(fmt.Sprintf("%d_dev", addr))
 
-	if request.Baud != 0 {
-		conn.Baud = request.Baud
-	}
-
-	if request.Timeout != 0 {
-		conn.ReadTimeout = request.Timeout
-	}
-
-	var err error
-
-	if conn.Dev == "" {
-
-
-		cache_key := cache.GetKey(fmt.Sprintf("%d_dev", request.Command[ADDRESS]))
-
-		//log.Println("send", request.Command)
-		//for i := 0; i<5; i++ {
-
-			cache_exist := cache.IsExist(cache_key)
-			if cache_exist {
-				conn.Dev = cache.Get(cache_key).(string)
-				result.Result, err, result.ErrorCode = m.exec(conn, request)
-				if err == nil {
-					result.Device = conn.Dev
-					return nil
-				}
-			} else {
-
-				//
-				devices := serial.DeviceList()
-				for _, device := range devices {
-					conn.Dev = device
-					result.Result, err, result.ErrorCode = m.exec(conn, request)
-					if err == nil {
-						result.Device = device
-						return nil
-					}
-				}
-			}
-
-		//}
-	} else {
-		for i := 0; i<5; i++ {
-			result.Result, err, result.ErrorCode = m.exec(conn, request)
-			if err == nil {
-				result.Device = conn.Dev
-				return nil
-			}
+	if request.Device == "" {
+		if cache.IsExist(cacheKey) {
+			request.Device = cache.Get(cacheKey).(string)
 		}
 	}
 
-	if err != nil {
-		result.Error = err.Error()
-	}
+	done := make(chan []byte)
+	defer func() {
+		close(done)
+	}()
+
+	var iter int
+	Server.ClientQueue(addr,request.Device, func(thread *Thread) {
+		if iter > 0 {
+			return
+		}
+		iter++
+		//fmt.Println("callback", request.Command)
+		err := thread.Send(request, result)
+		if err == nil {
+			thread.Type = THREAD_SMARTBUS
+			cache.Put("node", cacheKey, thread.Dev)
+		}
+
+		done <- request.Command
+	})
+
+	<- done
 
 	return nil
-}
-
-func (m *Smartbus) exec(conn *serial.Serial, request *Request) (result string, err error, errcode string) {
-
-	// get cache
-	cache_key := cache.GetKey(fmt.Sprintf("%d_dev", request.Command[ADDRESS]))
-
-	if _, err = conn.Open(); err != nil {
-		//cache.Delete(cache_key)
-		errcode = "SERIAL_PORT_ERROR"
-		//log.Printf("error: %s - %s\r\n",conn.Dev, err.Error())
-		return
-	}
-	defer conn.Close()
-
-	modbus := &smartbus.Smartbus{Serial: conn}
-	var b []byte
-	if b, err = modbus.Send(request.Command); err != nil {
-		//cache.Delete(cache_key)
-		errcode = "MODBUS_LINE_ERROR"
-		//log.Printf("error: %s - %s\r\n",conn.Dev, err.Error())
-		return
-	}
-
-	result = hex.EncodeToString(b)
-	cache.Put("node", cache_key, conn.Dev)
-
-	// bug in the devices need timeout, need fix!!!
-	if request.Sleep != 0 {
-		time.Sleep(time.Millisecond * time.Duration(request.Sleep))
-	}
-
-	return
 }
 
 func init() {
 	cache = &Cache{
 		Cachetime: 3600,
-		Name: "node",
+		Name:      "node",
 	}
 }
