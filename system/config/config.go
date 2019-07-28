e35f9093226195578ebe6dc07c82098c64cbd5b8
praetorian <filippov.a@e154.ru>
2019-07-28 13:42:42 +0700

    docker

--- system/config/config.go
+++ system/config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
-	"encoding/json"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const path = "conf/config.json"
@@ -22,5 +25,58 @@ func ReadConfig() (conf *AppConfig, err error) {
 		return
 	}
 
+	checkEnv(conf)
+
 	return
+}
+
+func checkEnv(conf *AppConfig) {
+
+	if name := os.Getenv("NAME"); name != "" {
+		conf.Name = name
+	}
+
+	if topic := os.Getenv("TOPIC"); topic != "" {
+		conf.Topic = topic
+	}
+
+	if mqttKeepAlive := os.Getenv("MQTT_KEEP_ALIVE"); mqttKeepAlive != "" {
+		v, _ := strconv.ParseInt(mqttKeepAlive, 10, 32)
+		conf.MqttKeepAlive = int(v)
+	}
+
+	if mqttConnectTimeout := os.Getenv("MQTT_CONNECT_TIMEOUT"); mqttConnectTimeout != "" {
+		v, _ := strconv.ParseInt(mqttConnectTimeout, 10, 32)
+		conf.MqttConnectTimeout = int(v)
+	}
+
+	if mqttSessionsProvider := os.Getenv("MQTT_SESSIONS_PROVIDER"); mqttSessionsProvider != "" {
+		conf.MqttSessionsProvider = mqttSessionsProvider
+	}
+
+	if mqttTopicsProvider := os.Getenv("MQTT_TOPICS_PROVIDER"); mqttTopicsProvider != "" {
+		conf.MqttTopicsProvider = mqttTopicsProvider
+	}
+
+	if mqttUsername := os.Getenv("MQTT_USERNAME"); mqttUsername != "" {
+		conf.MqttUsername = mqttUsername
+	}
+
+	if mqttPassword := os.Getenv("MQTT_PASSWORD"); mqttPassword != "" {
+		conf.MqttPassword = mqttPassword
+	}
+
+	if mqttIp := os.Getenv("MQTT_IP"); mqttIp != "" {
+		conf.MqttIp = mqttIp
+	}
+
+	if mqttPort := os.Getenv("MQTT_PORT"); mqttPort != "" {
+		v, _ := strconv.ParseInt(mqttPort, 10, 32)
+		conf.MqttPort = int(v)
+	}
+
+	if serial := os.Getenv("SERIAL"); serial != "" {
+		conf.Serial = strings.Split(serial, ",")
+	}
+
 }
