225ea049a1df1ecd660c842dfb3287b23eb563cb
delta54 <support@e154.ru>
2019-11-26 00:52:29 +0700

    issues #26 mqtt server

--- system/mqtt/mqtt_client.go
+++ system/mqtt/mqtt_client.go
@@ -8,113 +8,76 @@ import (
 )
 
 type Client struct {
-	qos              byte
-	baseTopic, uri   string
-	clientId         string
-	client           MQTT.Client
-	brokerLoad       chan bool
-	brokerConnection chan bool
-	brokerClients    chan bool
-	pongChannel      chan struct{}
-	cfg              *MqttConfig
-	loadCount        int
-	connectionCount  int
-	clientsCount     int
-	handler          func(MQTT.Client, MQTT.Message)
-	lastPing         time.Time
-	reconnect        bool
-	quit             chan struct{}
+	qos             byte
+	baseTopic, uri  string
+	clientId        string
+	client          MQTT.Client
+	pongChannel     chan struct{}
+	cfg             *MqttConfig
+	loadCount       int
+	connectionCount int
+	clientsCount    int
+	handler         func(MQTT.Client, MQTT.Message)
+	reconnect       bool
+	quit            chan struct{}
 }
 
 func NewClient(cfg *MqttConfig, baseTopic string, qos byte,
 	handler func(MQTT.Client, MQTT.Message)) (client *Client, err error) {
 
-	// Instantiates a new Client
 	uri := fmt.Sprintf("tcp://%s:%d", cfg.ServerIp, cfg.Port)
+
+	client = &Client{
+		baseTopic:   baseTopic,
+		handler:     handler,
+		qos:         qos,
+		uri:         uri,
+		pongChannel: make(chan struct{}),
+		cfg:         cfg,
+		reconnect:   true,
+	}
 
 	clientId := fmt.Sprintf("node-%d-%d", os.Getpid(), time.Now().Unix())
 	opts := MQTT.NewClientOptions().
 		AddBroker(uri).
 		SetClientID(clientId).
-		SetKeepAlive(2 * time.Second).
-		SetPingTimeout(1 * time.Second).
+		SetKeepAlive(time.Duration(cfg.KeepAlive) * time.Second).
+		SetPingTimeout(5 * time.Second).
+		SetConnectTimeout(time.Duration(cfg.ConnectTimeout) * time.Second).
 		SetCleanSession(true).
 		SetUsername(cfg.MqttUsername).
-		SetPassword(cfg.MqttPassword)
+		SetPassword(cfg.MqttPassword).
+		SetOnConnectHandler(client.onConnect).
+		SetConnectionLostHandler(client.onConnectionLostHandler)
 
-	c := MQTT.NewClient(opts)
-
-	client = &Client{
-		baseTopic:        baseTopic,
-		handler:          handler,
-		qos:              qos,
-		client:           c,
-		uri:              uri,
-		brokerLoad:       make(chan bool),
-		brokerConnection: make(chan bool),
-		brokerClients:    make(chan bool),
-		pongChannel:      make(chan struct{}),
-		cfg:              cfg,
-		reconnect:        true,
-		quit:             make(chan struct{}),
-	}
-
-	go func() {
-		for ; ; {
-			select {
-			case <-client.brokerLoad:
-				client.loadCount++
-			case <-client.brokerConnection:
-				client.connectionCount++
-			case <-client.brokerClients:
-				client.clientsCount++
-			case <-client.quit:
-				return
-			}
-		}
-	}()
+	client.client = MQTT.NewClient(opts)
 
 	return
+}
+
+func (c *Client) onConnectionLostHandler(client MQTT.Client, e error) {
+
+	log.Debug("connection lost...")
+
+	c.unsubscribe()
+}
+
+func (c *Client) onConnect(client MQTT.Client) {
+
+	log.Debug("connected...")
+
+	if token := c.client.Subscribe(c.baseTopic+"/req", c.qos, c.handler); token.Wait() && token.Error() != nil {
+		log.Error(token.Error().Error())
+	}
 }
 
 func (c *Client) Connect() {
 
 	log.Infof("Connect to server %s", c.uri)
 
-//loop:
-	time.Sleep(time.Millisecond * 500)
-
-	log.Info("connect ....")
-
-	//if c.client == nil {
-	//	return
-	//}
-
-	if c.client.IsConnected() {
-		c.client.Disconnect(250)
-	}
-
-	if c.client.IsConnected() {
-		return
-	}
-
 	if token := c.client.Connect(); token.Wait() && token.Error() != nil {
 		log.Error(token.Error().Error())
-		//goto loop
 	}
-
-	if token := c.Subscribe(c.baseTopic+"/req", c.qos, c.handler); token.Wait() && token.Error() != nil {
-		log.Error(token.Error().Error())
-		//goto loop
-	}
-
-	if token := c.Subscribe(c.baseTopic+"/pong", c.qos, c.pong); token.Wait() && token.Error() != nil {
-		log.Error(token.Error().Error())
-		//goto loop
-	}
-
-	c.lastPing = time.Now()
-	c.reconnect = false
 
 	return
 }
@@ -124,19 +87,15 @@ func (c *Client) Disconnect() {
 		return
 	}
 
-	c.quit <- struct{}{}
+	c.unsubscribe()
+	c.client.Disconnect(250)
+	c.client = nil
+}
 
+func (c *Client) unsubscribe() {
 	if token := c.client.Unsubscribe(c.baseTopic + "/req"); token.Error() != nil {
 		log.Error(token.Error().Error())
 	}
-
-	if token := c.client.Unsubscribe(c.baseTopic + "/pong"); token.Error() != nil {
-		log.Error(token.Error().Error())
-	}
-
-	c.client.Disconnect(250)
-
-	c.client = nil
 }
 
 func (c *Client) Publish(topic string, payload interface{}) (err error) {
@@ -146,25 +105,6 @@ func (c *Client) Publish(topic string, payload interface{}) (err error) {
 	return
 }
 
-func (c *Client) Subscribe(topic string, qos byte, handler func(MQTT.Client, MQTT.Message)) MQTT.Token {
-	return c.client.Subscribe(topic, qos, handler)
-}
-
 func (c *Client) IsConnected() bool {
-	//if c.reconnect {
-	//	return false
-	//}
-
-	k := time.Now().Sub(c.lastPing).Seconds()
-	if ok := k < 5; !ok {
-		//c.reconnect = true
-		//c.Connect()
-		return false
-	}
-
-	return true
-}
-
-func (c *Client) pong(MQTT.Client, MQTT.Message) {
-	c.lastPing = time.Now()
+	return c.client.IsConnectionOpen()
 }
